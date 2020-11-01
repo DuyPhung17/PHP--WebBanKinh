5d61bab4884e698eba99f37f9e7ebfcbca2c6ae4
DuyPhung99 <baduyrino8k@gmail.com>
2020-11-01 12:17:10 +0700

    them detail

---
+++ code.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop >= 500 || document.documentElement.scrollTop >= 500) {
+    document.getElementById("navbar").style.padding = "3px";
+    document.getElementById("logo").style.width = "auto";
+    document.getElementById("logo").style.height = "30px";
+  } else {
+    document.getElementById("navbar").style.padding = "15px";
+    document.getElementById("logo").style.width = "auto";
+    document.getElementById("logo").style.height = "50px";
+  }
+}
